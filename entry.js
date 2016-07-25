ebd7e6529b577e2b073ae206aae308f8c6d4cff9
Ryan Meier <ryanhurley@gmail.com>
2016-07-25 09:45:56 -0400

    fixes for android release builds

---
+++ entry.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import FormGenerator from './index'
+
+import {
+  Form,
+  Separator, InputField, LinkField,
+  SwitchField, PickerField, DatePickerField,
+  TimePickerField
+} from './index'
+  		  
+export {
+  Form,
+  Separator, InputField, LinkField,
+  SwitchField, PickerField, DatePickerField,
+  TimePickerField
+};
